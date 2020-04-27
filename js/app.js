ad8253be83fa8dec8b55e3e0523d66774c437a1c
Horus98 <42451015+Horus98@users.noreply.github.com>
2020-04-27 12:09:04 -0300

    Se agrega el cambio de tema de la página.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 var boton = document.getElementById("botonCalcular");
 var velocidad = document.getElementById("velocidadDescarga");
 var tamanio = document.getElementById("tamArchivo");
+var tablaValoresAnterioresBody = document.getElementById("tablaValoresAnterioresBody");
+
+
+const valoresAlmacenados = 5;
+const clave = "claveAlmacenamiento";
+
 // Velocidades del redes moviles expresadas en MBps.
 const velocidad5G = 128
 const velocidad4G = 3.75
 const velocidad3G = 0.625
+init();
+
+
 // Obtiene el valor del radio button checkeado dado un formulario.
 function obtenerValorCheckeado(formulario) {
 	for (var i = 0; i < formulario.length; i++) {
@@ -14,6 +23,7 @@ function obtenerValorCheckeado(formulario) {
   return formulario[i].value ;
 }
 
+// Calcula el tiempo requerido para descargar un archivo de tamaño "tam" a una velocidad "vel".
 function calcularTiempo(tam,vel){
 	var time = tam / vel;
 	var hours = Math.floor( time / 3600 );  
@@ -31,6 +41,7 @@ function calcularTiempo(tam,vel){
   return result;
 }
 
+// Verifica que la entrada sea valida.
 function verificarEntrada(entrada){
 	
 	for (var i = 0; i < entrada.length; i++){
@@ -39,7 +50,6 @@ function verificarEntrada(entrada){
 	}
 	return true;
 }
-
 
 boton.onclick = function() {
 	var formularioDescarga = document.forms[0].descarga;
@@ -67,13 +77,114 @@ boton.onclick = function() {
 		var tiempoVelocidad4G = calcularTiempo(tam,velocidad4G);
 		var tiempoVelocidad3G = calcularTiempo(tam,velocidad3G);
 
+		var cadenaStorage = armarCadena(vel,tam,tiempoVelocidadUsuario);
+		storeCadena(cadenaStorage);
+
 		document.getElementById("vel3g").innerHTML = tiempoVelocidad3G;
 		document.getElementById("vel4g").innerHTML = tiempoVelocidad4G;
 		document.getElementById("vel5g").innerHTML = tiempoVelocidad5G;
 		document.getElementById("tuVelocidad").innerHTML = tiempoVelocidadUsuario;
-
+		actualizarTablaValores();
 		velocidad.value = "";
 		tamanio.value = "";
 	}
 
-} 
+}
+
+// Almacena una cadena en el localStorage.
+function storeCadena(cadena) {  
+	var cadenaArray = getCadenas();
+	if(cadenaArray !== null) {
+		insertar(cadenaArray,cadena);
+		localStorage.setItem(clave, JSON.stringify(cadenaArray));
+	} else
+		alert("El localStorage no está habilitado en su navegador");   
+}
+
+// Devuelve un arreglo con las cadenas almacenadas en el localStorage
+function getCadenas() {
+    if (typeof(Storage) !== "undefined") {  
+        var cadenaArray = JSON.parse(localStorage.getItem(clave));
+        if(cadenaArray == null)
+            cadenaArray = [];
+        return cadenaArray;
+    }
+    return null;
+}
+
+// Almacena la cadena en el arreglo. En caso de que se almacenen mas del permitido se las elimina.
+function insertar(array,cadena){
+	array.push(cadena);
+    while(array.length > valoresAlmacenados)
+        array.shift();
+}
+
+// Inicializa la tablaValoresAnteriores.
+function init(){
+	actualizarTablaValores();
+	actualizarTema();
+}
+
+// Arma la cadena para luego ser mostrada en la tablaValoresAnteriores.
+function armarCadena(vel,tam,tiempoVelocidadUsuario) {
+	vel = vel.toFixed(1);
+	tam = tam.toFixed(1);
+	var velocidad = "Velocidad: "+vel+"MBps || ";
+	var tamanio = "";
+	if( tam > 1024){
+		tam = (tam / 1024).toFixed(1);
+		tamanio = "Tamaño: "+tam+"GB ";
+	}
+	else
+		tamanio = "Tamaño: "+tam+"MB || ";
+	var cadena = velocidad + tamanio +"Tiempo: " + tiempoVelocidadUsuario;
+	return cadena;
+}
+
+function almacenarCadena(arr){
+	localStorage.setItem(clave,arr);
+}
+
+// Actualiza la tabla donde se visualizan los valores del localStorage.
+function actualizarTablaValores (){
+	var cadenaArray = getCadenas();
+	if(cadenaArray !== null){
+		while(tablaValoresAnterioresBody.hasChildNodes()){
+		tablaValoresAnterioresBody.removeChild(tablaValoresAnterioresBody.firstChild);
+		}
+
+		for(var i = cadenaArray.length -1; i >= 0 ; i--){
+			var tr = document.createElement('TR');
+			tr.setAttribute("class","fila");
+			var td = document.createElement('TD');
+			td.setAttribute("class","columna");
+			td.appendChild(document.createTextNode(cadenaArray[i]));
+			tr.appendChild(td);
+			tablaValoresAnterioresBody.appendChild(tr);
+		}
+}
+}
+
+const btnSwitch = document.getElementById("switch");
+
+
+
+btnSwitch.onclick = function () {
+	var classList  = document.body.classList;
+	classList.toggle('dark');
+	// Guardamos en el localStorage.
+	if (classList.contains('dark')){
+		localStorage.setItem('dark-mode','true');
+	}
+	else
+		localStorage.setItem('dark-mode','false');
+
+}
+
+function actualizarTema () {
+	var classList = document.body.classList;
+	if(localStorage.getItem('dark-mode') === 'true')
+		classList.add('dark');
+	else
+		classList.remove('dark');
+}
